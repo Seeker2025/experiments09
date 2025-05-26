1de982529243423bd31381e3d6cd256a709d9028
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-05-26 13:45:54 +0300

    go on

--- src/js/page02.js
+++ src/js/page02.js
@@ -1,21 +1,50 @@
 console.log('page 02');
 
 import '../css/common.css'
+// import pokemonCardTpl from '../tamlates/pokemon-cards.hbs';
+
+function pokemonCardTpl(pokemon){
+    return `
+    <div class="card">
+          <div class="card-img-top">
+            <img src="${pokemon.sprites.front_default}" alt="{{name}}">
+          </div>
+
+    </div>
+    `
+};
 
 const refs ={
     cardConainer: document.querySelector('.js-card-container'),
 };
+// console.log(refs.cardConainer);
 
-fetch('https://pokeapi.co/api/pokemon/2')
-    .then(response =>{
-        return response.json();
-    })
-    .then(pokemon =>{
-        console.log(pokemon);
-        const markup = pokemonCardTpl(pokemon);
-        refs.cardConainerardContainer.innerHTML = markup;
-    })
-    .catch(error =>{
-        console.log(error);
-    })
+const r = fetch('https://pokeapi.co/api/v2/pokemon/2')
+    // .then(response =>{
+    //     return response.json();
+    // })
+    // .then(pokemon =>{
+    //     console.log(pokemon);
+    //     const markup = pokemonCardTpl(pokemon);
+    //     refs.cardConainer.innerHTML = markup;
+    // })
+    // .catch(error =>{
+    //     console.log(error);
+    // })
+r.then(response=>{
+    // console.log(response.json());
+    return response.json();
+})
+.then(pokemon=>{
+        //  console.log(pokemon);
+        //  console.log(pokemon.name);
+        //  console.log(pokemon.sprites.back_default);
+    
+    const markup = pokemonCardTpl(pokemon);
+    refs.cardConainer.innerHTML = markup;
+ })
+.catch(error=>{
+    console.log(error);
+ })
+    
     
