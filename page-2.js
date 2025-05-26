e18d282a1b8c0cca02d23e3846a4a40ad8d53103
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-05-26 10:46:18 +0000

    Deploying to gh-pages from @ Seeker2025/experiments09@1de982529243423bd31381e3d6cd256a709d9028 🚀

--- page-2.js
+++ page-2.js
@@ -1,2 +1,9 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                         */console.log("page 02");const e={cardConainer:document.querySelector(".js-card-container")};fetch("https://pokeapi.co/api/pokemon/2").then(o=>o.json()).then(o=>{console.log(o);const n=pokemonCardTpl(o);e.cardConainerardContainer.innerHTML=n}).catch(o=>{console.log(o)});
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                         */console.log("page 02");function r(o){return`
+    <div class="card">
+          <div class="card-img-top">
+            <img src="${o.sprites.front_default}" alt="{{name}}">
+          </div>
+
+    </div>
+    `}const e={cardConainer:document.querySelector(".js-card-container")},t=fetch("https://pokeapi.co/api/v2/pokemon/2");t.then(o=>o.json()).then(o=>{const n=r(o);e.cardConainer.innerHTML=n}).catch(o=>{console.log(o)});
 //# sourceMappingURL=page-2.js.map
