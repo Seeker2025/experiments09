110b607bfd1c9ea4d496a91dd86bb651b52c797f
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-05-30 11:16:28 +0300

    go on

--- src/js/page03.js
+++ src/js/page03.js
@@ -4,7 +4,6 @@ console.log('page 03 Add color square');
 
 const arr=[1, 2, 3,];
 // const arr02=[arr.indexOf(1), arr.indexOf(2), arr.indexOf(3)];
-
 function addToArr(){
     arr.shift();
     arr.push(arr[arr.length-1]+1);
@@ -25,53 +24,58 @@ addToArr(); ////// [5, 6, 7]
 /////// /////// /////
 */
 
-let colors = [
-    {id: 0, hex: '#f44336', rgb: '244,67,54', },
-    {id: 1, hex: '#C63E68', rgb: '198,62,104', },
-    {id: 2, hex: '#AC1E9E', rgb: '172,30,15', },
-    {id: 3, hex: '#3AE779', rgb: '58,231,121', },
-    {id: 4, hex: '#BE4B57', rgb: '190,75,87', },
-    {id: 5, hex: '#550943', rgb: '85,9,67', },
-    {id: 6, hex: '#B7E8EE', rgb: '183,232,238', },
-    {id: 7, hex: '#A0F00E', rgb: '160,240,14', },
-    {id: 8, hex: '#4F1AC5', rgb: '79,26,197', },
-    {id: 9, hex: '#657E3E', rgb: '101,126,62', },
+const colors = [
+    {id: '00', hex: '#f44336' },
+    {id: '01', hex: '#C63E68' },
+    {id: '02', hex: '#AC1E9E' },
+    {id: '03', hex: '#3AE779' },
+    {id: '04', hex: '#BE4B57' },
+    {id: '05', hex: '#550943' },
+    {id: '06', hex: '#B7E8EE' },
+    {id: '07', hex: '#A0F00E' },
+    {id: '08', hex: '#4F1AC5' },
+    {id: '09', hex: '#657E3E' },
+    {id: '10', hex: '#FF0000' },
+    {id: '11', hex: '#FF8C00' },
+    {id: '12', hex: '#FFFF00' },
 ];
 
 const contSqu = document.querySelector('.container_page_03');
 const btn = document.querySelector('.btn_next_color');
 
 let lineString = null;
-    btn.addEventListener('click', toShift)
+    btn.addEventListener('click', toShift);
+
+    // console.log(colors);
+
+    lineString = addSquare(colors);
+    contSqu.insertAdjacentHTML('beforeend', lineString);  
 
     function toShift(){
-        if(colors.lenth!==0) console.log('peace');
+      
+        contSqu.textContent = '';   
          
-    let theOne = colors.shift();
-    // console.log(theOne);
-    colors.push(theOne);
-    
-    
-    console.log(colors);
-// console.log(ooo);
- }
+        let theOne = colors.shift();
+        colors.push(theOne);
+        // console.log(theOne);
+        console.log(colors);
 
+        lineString = addSquare(colors);
+        contSqu.insertAdjacentHTML('beforeend', lineString);  
+     }
 //  toShift();
 //  toShift();
 //  toShift();
 
-// console.log(btn);
  function addSquare(colors){
     let strRef;
-    return strRef = colors.map((itm, idx)=>{
-        
-    return `<div class="otherSquare" style = "background-color: ${itm.hex}">${itm.id}</div>`
+ return strRef = colors.map((itm, idx)=>{
+ return `<div class="otherSquare" style = "background-color: ${itm.hex}">${itm.id}</div>`
       
 }).join('');
 }
 
-lineString = addSquare(colors);
-contSqu.insertAdjacentHTML('beforeend', lineString);   
+
 
 
 
