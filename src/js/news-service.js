74e58f3122122d205eb5deb31f9c4a670946ca86
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-06-06 19:00:53 +0300

    go on

---
+++ src/js/news-service.js
@@ -0,0 +1,36 @@
+export default class NewsApiService{
+    constructor(){
+        this.searchQuery='';
+        this.page = 1;
+
+    }
+    fetchArticles(){
+        console.log(this);
+        const options = {
+    headers: {
+        Authorization: 'c7cfa9764acc40e0977baf4dd8dae782',
+    }
+};
+    const url = `https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;
+   
+    console.log('Hi!');
+        fetch(url, options)
+            .then(r => r.json())
+            .then(data=>{
+                console.log(data);
+                this.incrementPage();
+            
+            });
+    }
+
+    incrementPage(){
+        this.page += 1;
+    }
+
+    get query(){
+        return this.searchQuery;
+    }
+    set query(newQuery){
+        this.searchQuery = newQuery;
+    }
+}
--- src/js/page04.js
+++ src/js/page04.js
@@ -1,27 +1,50 @@
 console.log('page 04');
 
+import NewsApiService from './news-service';
+// console.log(NewsApiService);
+
 const refs = {
     searchForm: document.querySelector('.js-search-form'),
     articlesContainer: document.querySelector('.js-articles-container'),
+    loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 };
-
-refs.searchForm.addEventListener('submit', onSearch);
-
-function onSearch(e){
-    e.preventDefault();
-    console.log('Hi!');
-}
 
 console.log(refs.searchForm);
 console.log(refs.articlesContainer);
+console.log(refs.loadMoreBtn);
 
-const options = {
-    headers: {
-        Authorization: 'c7cfa9764acc40e0977baf4dd8dae782',
-    }
-};
 
-const url = 'https://newsapi.org/v2/everything?q=cat&language=en&pageSize=10&page=1';
+const newsApiService = new NewsApiService();
 
-fetch(url, options)
-.then(r => r.json()).then(console.log());
+// const options = {
+//     headers: {
+//         Authorization: 'c7cfa9764acc40e0977baf4dd8dae782',
+//     }
+// };
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e){
+    e.preventDefault();
+
+    newsApiService.query = e.currentTarget.elements.query.value;
+    // const url = `https://newsapi.org/v2/everything?q=${searchQuery}&language=en&pageSize=5&page=1`;
+   
+    // console.log('Hi!');
+    // fetch(url, options)
+    // .then(r => r.json()).then(console.log());
+
+    newsApiService.fetchArticles();
+}
+
+function onLoadMore(){
+    newsApiService.fetchArticles();
+}
+
+
+
+
+
+
+
