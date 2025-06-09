6689ba092edae9d9204e5ab90cebf4aaf8201a24
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-06-09 15:35:41 +0000

    Deploying to gh-pages from @ Seeker2025/experiments09@96fb88aa6c1dfe3606096312ee54020b32ed2f84 🚀

--- page-4.js
+++ page-4.js
@@ -1,13 +1,13 @@
-import"./assets/styles-DVYB7QxO.js";class o{constructor(){this.searchQuery="",this.page=1}fetchArticles(){console.log(this);const t={headers:{Authorization:"c7cfa9764acc40e0977baf4dd8dae782"}},n=`https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;return fetch(n,t).then(a=>a.json()).then(a=>(this.incrementPage(),a.articles))}incrementPage(){this.page+=1}resetPage(){this.page=1}get query(){return this.searchQuery}set query(t){this.searchQuery=t}}const i={articlesContainer:document.querySelector(".js-articles-container")};function s(e){const t=`
+import"./assets/styles-BqxtG7N5.js";const l="c7cfa9764acc40e0977baf4dd8dae782";class h{constructor(){this.searchQuery="",this.page=1}fetchArticles(){console.log(this);const e={headers:{Authorization:l}},r=`https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;return fetch(r,e).then(s=>s.json()).then(s=>(this.incrementPage(),s.articles))}incrementPage(){this.page+=1}resetPage(){this.page=1}get query(){return this.searchQuery}set query(e){this.searchQuery=e}}class o{constructor({selector:e}){this.refs=this.getRefs(e)}getRefs(e){const r={};return r.button=document.querySelector(e),r}show(){this.refs.button.classList.remove("is-hidden")}hide(){this.refs.button.classList.add("is-hidden")}}const a={articlesContainer:document.querySelector(".js-articles-container")};function c(t){const e=`
         <li class="li_back">
-            <a href="${e.url}" target ="blank" rel="noopener noreferrer">
+            <a href="${t.url}" target ="blank" rel="noopener noreferrer">
                 <article>
-                    <img src="${e.urlToImage}" alt="" width="480" >
-                    <h2 class="title_h2">${e.title}</h2>
-                    <p>Posted by: ${e.author}</p>
-                    <p>${e.description}</p>
+                    <img src="${t.urlToImage}" alt="" width="480" >
+                    <h2 class="title_h2">${t.title}</h2>
+                    <p>Posted by: ${t.author}</p>
+                    <p>${t.description}</p>
                 </article>
             </a>
         </li>
-`;return i.articlesContainer.insertAdjacentHTML("beforeend",t)}console.log("page 04");const c={searchForm:document.querySelector(".js-search-form"),loadMoreBtn:document.querySelector('[data-action="load-more"]')},r=new o;c.searchForm.addEventListener("submit",h);c.loadMoreBtn.addEventListener("click",l);function h(e){e.preventDefault(),r.query=e.currentTarget.elements.query.value,r.resetPage(),r.fetchArticles().then(t=>{t.map(n=>{s(n)})})}function l(){r.fetchArticles().then(e=>{e.map(t=>{s(t)})})}
+`;return a.articlesContainer.insertAdjacentHTML("beforeend",e)}function u(){a.articlesContainer.innerHTML=""}console.log("page 04");console.log(o);const i={searchForm:document.querySelector(".js-search-form"),loadMoreBtn:document.querySelector('[data-action="load-more"]')},n=new h;new o({selector:'[data-action="load-more"]'});i.searchForm.addEventListener("submit",d);i.loadMoreBtn.addEventListener("click",p);function d(t){if(t.preventDefault(),u(),n.query=t.currentTarget.elements.query.value,n.resetPage(),n.query===" ")return alert("Введіть щось");n.fetchArticles().then(e=>{e.map(r=>{c(r)})})}function p(){n.fetchArticles().then(t=>{t.map(e=>{c(e)})})}
 //# sourceMappingURL=page-4.js.map
--- page-5.js
+++ page-5.js
@@ -1,2 +1,2 @@
-import"./assets/styles-DVYB7QxO.js";console.log("page 05");
+import"./assets/styles-BqxtG7N5.js";console.log("page 05");
 //# sourceMappingURL=page-5.js.map
