c32b1e9a771191718e19f7a762d69ac0545b00fc
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-06-19 15:17:41 +0300

    go on

--- src/js/page06.js
+++ src/js/page06.js
@@ -1,1 +1,26 @@
 console.log('page 06');
+
+function getDate(){
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(response=>response.json())
+    .then(data => {
+        
+        const{results: dataArray}=data
+        console.log(dataArray);
+        const markUp = dataArray.map(item=>{
+            return  `
+            <li>
+               <img src="${item.image}" alt="">
+               <p>NAME:${item.name}</p>
+               <p>GENDER:${item.gender}</p>
+               <p>ID: ${item.id}</p>
+               <p>STATUS: ${item.status}</p>
+               <p>CREATED: ${item.created}</p>
+            </li>
+                `
+        }).join('');
+        document.querySelector('.characterList').innerHTML = markUp;
+})
+}
+
+getDate();
