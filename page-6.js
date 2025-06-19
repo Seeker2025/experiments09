8bcae63ffbb7e1011b0e8c0fe169d4f18938f060
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-06-19 12:17:54 +0000

    Deploying to gh-pages from @ Seeker2025/experiments09@c32b1e9a771191718e19f7a762d69ac0545b00fc 🚀

--- page-6.js
+++ page-6.js
@@ -1,2 +1,11 @@
-import"./assets/styles-jsnO26a3.js";console.log("page 06");
+import"./assets/styles-Nt7tdjrk.js";console.log("page 06");function n(){fetch("https://rickandmortyapi.com/api/character").then(r=>r.json()).then(r=>{const{results:t}=r;console.log(t);const e=t.map(a=>`
+            <li>
+               <img src="${a.image}" alt="">
+               <p>NAME:${a.name}</p>
+               <p>GENDER:${a.gender}</p>
+               <p>ID: ${a.id}</p>
+               <p>STATUS: ${a.status}</p>
+               <p>CREATED: ${a.created}</p>
+            </li>
+                `).join("");document.querySelector(".characterList").innerHTML=e})}n();
 //# sourceMappingURL=page-6.js.map
