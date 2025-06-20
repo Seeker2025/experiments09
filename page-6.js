bbb076f05afdb7674ea38224c9a50cad2b33dabf
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-06-20 11:40:03 +0000

    Deploying to gh-pages from @ Seeker2025/experiments09@ad3c68948dad6bf6bec092985c72b21679bbbfa6 🚀

--- page-6.js
+++ page-6.js
@@ -1,11 +1,11 @@
-import"./assets/styles-Nt7tdjrk.js";console.log("page 06");function n(){fetch("https://rickandmortyapi.com/api/character").then(r=>r.json()).then(r=>{const{results:t}=r;console.log(t);const e=t.map(a=>`
+import"./assets/styles-Nt7tdjrk.js";class o{constructor(c){this.toRick=this.toRick(c)}toRick(c){return console.log(c)}}console.log("page 06. Rick and Morty");const n=new o("Rick_02");n.markUpP06;function e(){fetch("https://rickandmortyapi.com/api/character").then(r=>r.json()).then(r=>{const{results:c}=r,a=c.map(t=>`
             <li>
-               <img src="${a.image}" alt="">
-               <p>NAME:${a.name}</p>
-               <p>GENDER:${a.gender}</p>
-               <p>ID: ${a.id}</p>
-               <p>STATUS: ${a.status}</p>
-               <p>CREATED: ${a.created}</p>
+               <img src="${t.image}" alt="">
+               <p>NAME:${t.name}</p>
+               <p>GENDER:${t.gender}</p>
+               <p>ID: ${t.id}</p>
+               <p>STATUS: ${t.status}</p>
+               <p>CREATED: ${t.created}</p>
             </li>
-                `).join("");document.querySelector(".characterList").innerHTML=e})}n();
+                `).join("");document.querySelector(".characterList").innerHTML+=a})}e();
 //# sourceMappingURL=page-6.js.map
