0fa5116fd7f4ca8b885d8586ed0e0710cbb89b99
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-06-22 09:22:10 +0000

    Deploying to gh-pages from @ Seeker2025/experiments09@88855ef163766fae80eda02829efaea42d7565d2 🚀

--- page-7.js
+++ page-7.js
@@ -1,2 +1,11 @@
-import"./assets/styles-SMsWUO0G.js";console.log("page 07. Infinite scroll");
+import"./assets/styles-SMsWUO0G.js";console.log("page 07. Infinite scroll");let c=1;function o(){fetch(`https://rickandmortyapi.com/api/character/?page=${c}`).then(e=>e.json()).then(e=>{const{results:n}=e,r=n.map(t=>`
+            <li>
+               <img src="${t.image}" alt="">
+               <p>NAME:${t.name}</p>
+               <p>GENDER:${t.gender}</p>
+               <p>ID: ${t.id}</p>
+               <p>STATUS: ${t.status}</p>
+               <p>CREATED: ${t.created}</p>
+            </li>
+                `).join("");document.querySelector(".characterList").innerHTML+=r,c++})}o();window.addEventListener("scroll",()=>{const{scrollHeight:e,scrollTop:n,clientHeight:r}=document.documentElement;o()});
 //# sourceMappingURL=page-7.js.map
