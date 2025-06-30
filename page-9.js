c3235254d905e06b4a377ec73bb4dfe51a40d46c
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-06-30 11:31:20 +0000

    Deploying to gh-pages from @ Seeker2025/experiments09@9163dd116449ec3918f2ad3bbe8f913581e96ada 🚀

--- page-9.js
+++ page-9.js
@@ -1,2 +1,2 @@
-import"./assets/styles-DQ82frmz.js";console.log("page 09");
+import"./assets/styles-DQ82frmz.js";console.log("page 09");const t="https://api.themoviedb.org/3/",n="trending/movie/day",e="3af5fd0b838bf2a259ff06cb75968037";function c(){return fetch(`${t}${n}?api_key=${e}`).then(o=>{if(!o.ok)throw new Error(o.statusText);return o.json()})}c().then(o=>console.log(o)).catch(o=>console.log(o));
 //# sourceMappingURL=page-9.js.map
