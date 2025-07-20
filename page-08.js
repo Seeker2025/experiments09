e52e116847ead3d3ba53a95227a065628d984d8b
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2025-07-20 09:00:44 +0000

    Deploying to gh-pages from @ Seeker2025/experiments09@90d1d93939bbf73724aa5835b76de2b83d27692c 🚀

--- page-08.js
+++ page-08.js
@@ -1,9 +1,9 @@
-import"./assets/styles-q4qmUVT2.js";console.log("page 08. Weather");const s=document.querySelector(".js-search"),a=document.querySelector(".js-list");s.addEventListener("submit",l);function l(t){t.preventDefault(),console.log(t.currentTarget.elements);const{query:n,days:o}=t.currentTarget.elements;i(n.value,o.value).then(e=>{console.log(e.forecast.forecastday),a.innerHTML="",u(e.forecast.forecastday)}).catch(e=>console.log(e))}function i(t,n){const o="https://api.weatherapi.com/v1",e="65fc8710104c48e595e140016252406",c=new URLSearchParams({key:e,q:t,days:n,lang:"uk"});return fetch(`${o}/forecast.json?${c}`).then(r=>{if(!r.ok)throw new Error(r.statusText);return r.json()})}function u(t){const n=t.map(({date:o,day:{avgtemp_c:e,condition:{text:c,icon:r}}})=>`
+import"./assets/styles-q4qmUVT2.js";console.log("page 08. Weather");const s=document.querySelector(".js-search"),a=document.querySelector(".js-list");s.addEventListener("submit",l);function l(t){t.preventDefault(),console.log(t.currentTarget.elements);const{query:c,days:o}=t.currentTarget.elements;console.log(o.value);let n=Number(o.value);console.log(typeof n),u(c.value,n).then(e=>{console.log(e.forecast.forecastday),a.innerHTML="",i(e.forecast.forecastday)}).catch(e=>console.log(e))}function u(t,c){const o="https://api.weatherapi.com/v1",n="65fc8710104c48e595e140016252406",e=new URLSearchParams({key:n,q:t,days:c,lang:"uk"});return fetch(`${o}/forecast.json?${e}`).then(r=>{if(!r.ok)throw new Error(r.statusText);return r.json()})}function i(t){const c=t.map(({date:o,day:{avgtemp_c:n,condition:{text:e,icon:r}}})=>`
             <li class="p8_li">
-                <img src="${r}" alt="${c}">
-                <p>${c}</p>
+                <img src="${r}" alt="${e}">
+                <p>${e}</p>
                 <h2>${o}</h2>
-                <h3>${e}</h3>
+                <h3>${n}</h3>
             </li>
-        `).join("");return a.insertAdjacentHTML("beforeend",n)}
+        `).join("");a.insertAdjacentHTML("beforeend",c)}
 //# sourceMappingURL=page-08.js.map
